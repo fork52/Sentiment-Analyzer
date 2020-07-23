b3fa2b512987853d93352a39d205975e7dd34b50
fork52 <mrunankmistry52@gmail.com>
2020-07-23 21:39:07 +0530

    Added tokenizer

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter.font import Font
 
 class GUI:
     def create_root(self):
@@ -7,14 +9,24 @@ class GUI:
         root.title('Review Sentiment Analyzer')
         root.geometry('700x630+350+50') # widthXheight topx+topy
         root.resizable(False, False)
+        root.configure(background='black')
         self.root = root
         return root
         # self.algo_choice = tk.IntVar(root,value=1)
+    
+    def add_textbox(self):
+        self.hv16 = Font(family="Helvetica", size=16)
+        sc = ScrolledText(root,font=self.hv16,bg='#c0effc')
+        sc.place(   
+             x = 50 ,y = 60 ,
+            width=600 , height = 200
+        )
+
 
 
 
 if __name__ == "__main__":
     window = GUI()
     root = window.create_root()
-    #Mainloop
+    window.add_textbox()
     root.mainloop()
---
+++ model_utils.py
@@ -0,0 +1,50 @@
+import logging
+from os import environ
+environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
+
+from typing import Iterable
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from numpy import argmax
+from pickle import load as pkl_load
+
+
+#Setting hyperparameters
+vocab_size = 20000
+embedding_dim = 32
+oov_tok = "<OOV>"
+trunc_type='post'
+max_length = 220
+padding_type='post'
+
+def prepare_input(X:list):
+    '''Convert input to'''
+    with open('models/tokenizer.pkl', 'rb') as f:
+        tokenizer = pkl_load(f)
+
+    sequences = tokenizer.texts_to_sequences(X)
+
+    padded = pad_sequences(
+                           sequences,
+                           maxlen=max_length,
+                           truncating=trunc_type,
+                           padding=padding_type
+                        )
+    return padded
+
+def get_rating(model_path:str , sentence:str ):
+    '''Takes as input path to model and a sentence.'''
+    model = load_model(model_path)
+    new_sent = [sentence]
+    new_data = prepare_input(new_sent)
+    rating = argmax( model.predict(new_data) ) + 1
+    return rating
+
+if __name__ == "__main__":
+    sentence = 'This is a decent and average product i guess.'
+    rating = get_rating('models/basic_model',sentence)
+    print('Rating given by model is:',rating)
+
+
